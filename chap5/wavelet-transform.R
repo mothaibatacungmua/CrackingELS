1c22eac7dd6bb42bd000da03473d5172ad37e29e
Duc Tran <duc20102638@gmail.com>
2016-12-01 21:49:31 +0700

    Fix wavelet transform

--- chap5/wavelet-transform.R
+++ chap5/wavelet-transform.R
@@ -19,18 +19,21 @@ haar_mother <- function(x, j, k){
 # plot some haar basis
 par(mfrow=c(3,3))
 
-x = seq(0,2, length.out = 64)
+x = seq(-2,2, length.out = 8192)
 b = haar_father(x, 0, 0)
 plot(x, b, type = 'l', main = 'Haar(0,0)')
 
+b = haar_mother(x, 0,0)
+plot(x, b, type = 'l', main = 'Haar(1,1)')
+
 b = haar_mother(x, 1,1)
-plot(x, b, type = 'l', main = 'Haar(2,1)')
+plot(x, b, type = 'l', main = 'Haar(1,1)')
 
 b = haar_mother(x, 1, 2)
-plot(x, b, type = 'l', main = 'Haar(2,2)')
+plot(x, b, type = 'l', main = 'Haar(1,2)')
 
 b = haar_mother(x, 2, 1)
-plot(x, b, type = 'l', main = 'Haar(2,2)')
+plot(x, b, type = 'l', main = 'Haar(2,1)')
 
 b = haar_mother(x, 2, 3)
 plot(x, b, type = 'l', main = 'Haar(2,3)')
@@ -44,19 +47,15 @@ plot(x, b, type = 'l', main = 'Haar(3,2)')
 b = haar_mother(x, 3, 3)
 plot(x, b, type = 'l', main = 'Haar(3,3)')
 
-b = haar_mother(x, 4, 1)
-plot(x, b, type = 'l', main = 'Haar(4,1)')
+b = haar_mother(x, 8, 1)
+plot(x, b, type = 'l', main = 'Haar(8,1)')
 
 
 # code for wavelet transforming
-father_coefF_expand = 1/(sqrt(2))
-mother_coeff_expand <- function(n){
-  t = n %% 2
-  ret = (-1)^t*father_coefF_expand
-  return(ret)
-}
+fcof = c(1/sqrt(2),1/sqrt(2))
+mcof = c(1/sqrt(2), -1/sqrt(2))
 
-N = 256
+N = 8
 J = log(N, 2)
 
 # using dynamic programming to do the transformation
@@ -66,31 +65,48 @@ basis_coeff <- function(y){
   ret_coeffs = rep(0, N)
   
   for(j in (J-1):0){
-    raw_coeffs[2^j] = father_coefF_expand*raw_coeffs[2^(j+1)]
-    ret_coeffs[2^j] = mother_coeff_expand(2^j) * raw_coeffs[2^(j+1)]
-    for(k in 1:(2^j-1)){
-        raw_coeffs[2^j + k] = father_coefF_expand*sum(raw_coeffs[2^(j+1):(2^(j+1) + 2*k - 1)])  
-        h = mother_coeff_expand(seq(0, 2*k-1))
-        ret_coeffs[2^j + k] = sum(h * raw_coeffs[2^(j+1):(2^(j+1) + 2*k - 1)])
+    for(k in 0:(2^j-1)){
+        raw_coeffs[2^j + 1 + k] = fcof[1]*(raw_coeffs[2^(j+1)+1 + 2*k]) + 
+                                  fcof[2]*(raw_coeffs[2^(j+1)+1 + 2*k + 1])
+        
+        ret_coeffs[2^j + 1 + k] = mcof[1]*(raw_coeffs[2^(j+1)+1 + 2*k]) + 
+                                  mcof[2]*(raw_coeffs[2^(j+1)+1 + 2*k + 1])
     }
   }
   
-  return(ret_coeffs)
+  ret_coeffs[1] = raw_coeffs[2]
+  
+  return(ret_coeffs*2^(-J/2))
 }
 
 transform_func <- function(x, coeffs){
   ret = coeffs[1] * father(x)
   
-  for(j in 1:(J-1)){
+  for(j in 0:(J-1)){
     for(k in 0:(2^j-1)){
-      ret = ret + coeffs[2^j + k] * haar_mother(x, j, k)
+      ret = ret + coeffs[2^j + 1 + k] * haar_mother(x, j, k)
     }
   }
   
-  return(ret/(sqrt(N)))
+  return(ret)
 }
 
-# testing
-x = seq(0,10, length.out = N)
-y = sin(x) + rnorm(N, 0, sd = 0.5)
-plot(x,y)
+# testing case
+N = 8
+J = log(N,2)
+x = seq(0,1,length.out = 8)
+y = c(1, 0, -3, 2, 1, 0, 1, 2)
+
+#coeffs should be c(1/2, -1/2, 1/(2*sqrt(2)), -1/(2*sqrt(2)), 1/4, -5/4, 1/4, -1/4)
+coeffs = basis_coeff(y)
+
+# using wavelet to reconstruct
+N = 256
+J = log(N, 2)
+x = seq(0,1, length.out = N)
+y = sin(50*x) + rnorm(N, 0, sd = 0.5)
+plot(x,y, type = 'l')
+
+coeffs = basis_coeff(y)
+y_hat = transform_func(x, coeffs)
+points(x, y_hat, type='l', col='red')
